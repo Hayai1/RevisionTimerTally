fea878ddd0e3880c2b9f694e437cc912d8240666
Dylan <dylanpjhough@gmail.com>
2024-04-01 15:32:52 +0100

    added stuff

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 @app.route('/')
 def index():
     hour = time.localtime(3600)
-    return render_template('main.html', time_limit_arg=3600)
+    return render_template('main.html', time_limit_arg=20, break_time_arg=20)
 
 
 if __name__ == '__main__':
