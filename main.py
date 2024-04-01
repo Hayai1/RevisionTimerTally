e7f4303d4bb299cf770957ddec3bb7c8d66935a9
Dylan <dylanpjhough@gmail.com>
2024-04-01 17:31:24 +0100

    couple of additions

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from flask import Flask
-from flask import render_template, url_for, jsonify
-import time
+from flask import render_template, url_for, jsonify, session, request
 app = Flask(__name__, template_folder='templates', static_folder='static')
+app.secret_key = 'your secret key'
 
+
+@app.route('/save_hours', methods=['POST'])
+def save_hours():
+    hours_completed = request.form.get('hoursCompleted')
+    session['hoursCompleted'] = hours_completed
+    return jsonify({'status': 'success'}), 200
+
+
+@app.route('/start_revision')
+def startRevision():
+    hours_completed = session.get('hoursCompleted', 0)
+    return render_template('main.html', hours_completed_arg=int(hours_completed), time_limit_arg=10, break_time_arg=5)
 
 @app.route('/')
 def index():
-    hour = time.localtime(3600)
-    return render_template('main.html', time_limit_arg=20, break_time_arg=20)
+    session.pop('hoursCompleted', None)
+    return render_template('revision.html')
+
 
 
 if __name__ == '__main__':
