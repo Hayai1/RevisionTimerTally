a510e2feaf0f26008c7ff994ab6f210dc210b8f5
Dylan <dylanpjhough@gmail.com>
2024-04-01 14:09:13 +0100

    init commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import render_template, url_for, jsonify
+import time
+app = Flask(__name__, template_folder='templates', static_folder='static')
+
+
+@app.route('/')
+def index():
+    hour = time.localtime(3600)
+    return render_template('main.html', time_limit_arg=3600)
+
+
+if __name__ == '__main__':
+    app.run(debug=True,)
